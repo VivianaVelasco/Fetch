d03eb5415573a00c3985c1ae33b917b34356ef30
VivianaVelasco <vivavela@espol.edu.ec>
2022-06-21 22:51:52 -0500

    guia8

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -5,24 +5,28 @@
 
 const cargarDatos = () => {
     fetch("https://dataserverdaw.herokuapp.com/escritores/xml")
-    .then(data => data.text())
-    .then(data => new DOMParser().parseFromString(data, "text/xml"))
-    .then(xml => {
-        let arreglo, elemento
-        elemento = document.getElementsByName("select")[0]
-        arreglo = xml.querySelectorAll("escritores > escritor")
-        console.log(elemento)
-        for(let escritor of arreglo) {
-          let opcion = document.createElement("option")
-          let valor = escritor.querySelector("nombre") == null ? "" : escritor.querySelector("nombre").textContent
-          option.textContent = valor
-          let id = escritor.querySelector("id") == null ? "" : escritor.querySelector("id").textContent
-          opcion.setAttribute("value", id)
-          elemento.appendChild(opcion)
+      .then((data) => data.text())
+      .then((data) => new DOMParser().parseFromString(data, "text/xml"))
+      .then((xml) => {
+        let arreglo, element;
+        element = document.getElementsByName("select")[0];
+        arreglo = xml.querySelectorAll("escritores > escritor");
+        for (let escritor of arreglo) {
+          let option = document.createElement("option");
+          let valor =
+            escritor.querySelector("nombre") == null
+              ? ""
+              : escritor.querySelector("nombre").textContent;
+          option.textContent = valor;
+          let id =
+            escritor.querySelector("id") == null
+              ? ""
+              : escritor.querySelector("id").textContent;
+          option.setAttribute("value", id);
+          element.appendChild(option)
         }
-    })    
-}
-
+      });
+  };
 
 const selector= document.querySelector("select");
 selector.addEventListener("change", () =>{
